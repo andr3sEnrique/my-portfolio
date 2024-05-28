224c0210f284f6b4cbb9a72bb411aec0fa54ea78
andr3sEnrique <20213tn080@utez.edu.mx>
2024-05-28 14:54:15 +0100

    second part of about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Papel from "../img/papel-picado.png";
-import Altar from "../img/altar.png"
+import Altar from "../img/altar.png";
 import profile from '../img/profile-pic.jpeg';
+import LogoUtez from '../img/Logo-utez.png';
+import LogoOrleans from '../img/logo-orleans.png';
 import '../styles/about.css';
-
 
 function About () {
     const [levelVisible, setLevelVisible] = useState(0);
@@ -12,22 +13,18 @@ function About () {
     useEffect(() => {
         const handleScroll = () => {
             const scrolled = window.scrollY;
-            // Adjust these values based on the height/spacing of your levels
             if (scrolled < 130) {
                 setScale(1 + scrolled / 100);
                 setLevelVisible(0);
-            } else if (scrolled >= 130 && scrolled < 320) {
+            } else if (scrolled >= 130 && scrolled < 300) {
                 setLevelVisible(1);
-            } else if (scrolled >= 320 && scrolled < 520) {
-                setLevelVisible(2);
-            } else if (scrolled >= 520) {
-                setLevelVisible(3);
-            }
+            } 
         };
 
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
+
     return (
         <div>
             <img className="papel" src={Papel} alt="papel picado" />
@@ -41,24 +38,42 @@ function About () {
                         <span className="white-text">Enrique Ortiz </span>
                         <span className="red-color">Santa Cruz?</span>
                     </h1>
-                    <div className="d-flex justify-content-center ">
+                    <div className="d-flex justify-content-center content-1">
                         <img src={profile} alt="Enrique's profile" className="profile-pic"/>
                         <div className="line"></div>
                         <div className="d-flex justify-content-center flex-column">
                             <p className="text-google">Hello, I'm Andres Enrique Ortiz Santa Cruz, a <span className="key-words">software development</span> student who likes to create web and mobile applications.</p>
-                            <p className="text-google">When I'm not working I like to <span className="key-words">work out</span> at the gym, 
-                            <span className="key-words">explore</span> and <span className="key-words">learn</span> new things.</p>
+                            <p className="text-google">When I'm not working I like to <span className="key-words">work out</span> at the gym, <span className="key-words">explore</span> and <span className="key-words">learn</span> new things.</p>
                         </div>
                     </div>
-                </div>
-                <div className={`altar-level level-2 ${levelVisible === 2 ? 'visible' : 'hidden'}`}>
-                    
-                </div>
-                <div className={`altar-level level-3 ${levelVisible === 3 ? 'visible' : 'hidden'}`}>
-                    nvierfrelfle
+                    <h1 className="title">Academic Formation</h1>
+                    <div className="row mt-4">
+                        <div className="col">
+                            <img alt="Logo utez" src={LogoUtez} className="logos me-2" />
+                           
+                        </div>
+                        <div className="col">
+                            <h2>Universidad Técnologica Emiliano Zapata - <a href="http://www.utez.edu.mx" target="blanc" className="reference">UTEZ</a></h2>
+                            <p className="text-google">University Technician in multiplatform software development.</p>
+                        </div>
+                    </div>
+                    <div className="row mt-4">
+                        <div className="col">
+                            <img alt="Logo orleans" src={LogoOrleans} className="logos me-2" />
+                        </div>
+                        <div className="col">
+                            <h2>Université d'Orléans - <a className="reference" target="blanc" href="https://www.univ-orleans.fr/fr/iut-orleans">IUT Orléans</a></h2>
+                            <p className="text-google">IT B.U.T - Realization of applications: design, development, validation.</p>
+                        </div>
+                            
+                    </div>
+
+                    <h1 className="title">Hobbies</h1>
+                    <div>
+                        <h1>More content here</h1>
+                    </div>
                 </div>
             </div>
-            
         </div>
     );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import imgHome from '../img/imgHome.jpg';
+import { Link } from 'react-router-dom';
 import '../styles/home.css'
 function Home() {
     return (
@@ -8,7 +9,7 @@ function Home() {
                     <h1 className='text-google mt-5'>Hey, I'm <span className='name'>Enrique</span> <span className='wave-emoji'>👋🏽</span></h1>
                     <p className='text-google introduction mt-4'>I am a Mexican student developer at the IUT in Orléans. I have done different projects using a variety of technologies and tools.</p>
                 <div className='d-flex justify-content-center mt-5 align-items-center'>
-                    <h2>Let me show you more <a className='reference-a' href='/about'>about me</a></h2>
+                    <h2>Let me show you more <Link to="/about" className='reference-a'>about me</Link></h2>
                     <img className='img-home' src={imgHome} alt='Anime Software' />
                 </div>
 
